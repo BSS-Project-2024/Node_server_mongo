24447166f1cf1e4fb104b2c8c152d53520028e45
dhruvil <dhruvilun522@gmail.com>
2024-04-23 16:29:23 +0530

    added domain finding from url

--- models/Item.js
+++ models/Item.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const itemSchema = new mongoose.Schema({
     domain: {
         type: String,
-        required: true,
+        required: false,
     },
     web_url:{
         type: String,
--- routes/postItems.js
+++ routes/postItems.js
@@ -1,11 +1,15 @@
 const Item = require('../models/Item');
-
+const { URL } = require('url');
 const postItems = async (req, res) => {
     try {
         // Create a new item using the data from the request body
         const newItem = new Item(req.body);
-        
+        const urlObject = new URL(newItem.web_url);
+
+// Extract the domain name (hostname)
+const domainName = urlObject.hostname;
         // Save the new item to the database
+        newItem.domain=domainName;
         await newItem.save();
         
         // Send a response with the saved item
