90bbb25fc8efbfffc1bb3e0f53874e61e1963d6f
dhruvil <dhruvilun522@gmail.com>
2024-04-23 16:47:28 +0530

    minor changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
