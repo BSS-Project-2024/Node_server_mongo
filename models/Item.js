b6c3233304db4caf566adf205b0fde1b318fdb2f
utsavchab <21je1011@iitism.ac.in>
2024-08-30 22:20:31 +0530

    leaked scerets

--- models/Item.js
+++ models/Item.js
@@ -1,24 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const itemSchema = new mongoose.Schema({
-    domain: {
-        type: String,
-        required: false,
-    },
-    web_url:{
-        type: String,
-        required: true
-    },
-    text: {
-        type: String,
-        required: true,
-    },
-    timestamp: {
-        type: Date,
-        default: Date.now,
-    }
-});
+  domain: {
+    type: String,
+    required: false,
+  },
+  web_url: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+})
+itemSchema.index({ text: 1, web_url: 1 }, { unique: true })
 
-const Item = mongoose.model('Item', itemSchema);
+const Item = mongoose.model('Item', itemSchema)
 
-module.exports = Item;
+module.exports = Item
--- routes/postItems.js
+++ routes/postItems.js
@@ -1,23 +1,53 @@
-const Item = require('../models/Item');
-const { URL } = require('url');
+const Item = require('../models/Item')
+
 const postItems = async (req, res) => {
-    try {
-        // Create a new item using the data from the request body
-        const newItem = new Item(req.body);
-        const urlObject = new URL(newItem.web_url);
+  try {
+    // Create a new item using the data from the request body
+    console.log(req.body)
+    const { predicted_list, web_url } = req.body
+    const url = new URL(web_url)
+    const domain = url.hostname
 
-// Extract the domain name (hostname)
-const domainName = urlObject.hostname;
-        // Save the new item to the database
-        newItem.domain=domainName;
-        await newItem.save();
-        
-        // Send a response with the saved item
-        res.status(201).json(newItem);
-    } catch (err) {
-        // Handle any errors during the saving process
-        res.status(400).json({ error: 'Failed to insert item', details: err.message });
+    const fileName = url.pathname.split('/').pop()
+
+    if (predicted_list.length == 0) {
+      return res.status(200).json('No dark pattern Inserted')
     }
-};
+    const data = predicted_list.map((darkPattern) => {
+      return {
+        text: darkPattern.text,
+        web_url,
+        domain: domain || fileName,
+      }
+    })
+    console.log(data)
 
-module.exports = postItems;
+    const output = []
+    for (const item of data) {
+      try {
+        const presentData = await Item.findOne({
+          web_url: web_url,
+          text: item.text,
+        })
+        if (presentData) {
+        } else {
+          const newItem = new Item(item)
+          const savedItem = await newItem.save()
+          output.push(savedItem)
+        }
+      } catch (err) {
+        console.log(`Failed to insert item: ${item.text}`, err)
+      }
+    }
+
+    res.status(201).json(output)
+  } catch (err) {
+    // Handle any errors during the saving process
+    console.log(err)
+    res
+      .status(400)
+      .json({ error: 'Failed to insert item', details: err.message })
+  }
+}
+
+module.exports = postItems
